e9eff22e90401f838f6fd7850248534922f06f41
vlakondra <kondrashkin.wladimir@yandex.ru>
2024-10-30 00:00:37 +0500

    first comm

---
+++ src/App.svelte
@@ -0,0 +1,89 @@
+<script>
+  let display = $state([
+    "block",
+    "grid",
+    "inline-block",
+    "flex",
+    "inline-flex",
+    "none",
+  ]);
+
+  let flex_direction = $state([
+    "row",
+    "row-reverse",
+    "column",
+    "column-reverse",
+  ]);
+
+  let just_content = $state([
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+  ]);
+
+  let disp_selected = $state("block");
+  let direct_selected = $state("row");
+  let just_content_selected = $state("flex-start");
+
+  import FlexShow from "./lib/FlexShow.svelte";
+</script>
+
+<main>
+  <h1>START</h1>
+
+  <div class="combos">
+    <div>
+      <select bind:value={disp_selected}>
+        {#each display as item}
+          <option>
+            {item}
+          </option>
+        {/each}
+      </select>
+    </div>
+
+    <div>
+      <select bind:value={direct_selected}>
+        {#each flex_direction as item}
+          <option>
+            {item}
+          </option>
+        {/each}
+      </select>
+    </div>
+
+    <div>
+      <select bind:value={just_content_selected}>
+        {#each just_content as item}
+          <option>
+            {item}
+          </option>
+        {/each}
+      </select>
+    </div>
+
+  </div>
+<!-- Дочернему компоненту передаются значения props'ов и переменных стиля -->
+  <div>
+    <FlexShow
+      --disp={disp_selected}
+      --direct={direct_selected}
+      --justcont ={just_content_selected}
+      display={disp_selected}
+      flex_dir={direct_selected}
+      just_cont={just_content_selected}
+    />
+  </div>
+</main>
+
+<style>
+  .combos {
+    display: flex;
+  }
+  main{
+    margin:auto;
+    width:50%;
+  }
+</style>
---
+++ src/lib/FlexShow.svelte
@@ -0,0 +1,54 @@
+<script>
+  let props = $props();
+
+  //вызывается при изменении любого props'a 
+  //и обновляет значения массива для распечатки стиля
+  $effect(() => {
+    stl.display = props.display;
+    stl["flex-direction"] = props.flex_dir;
+    stl["justify-content"] = props.just_cont;
+  });
+
+  //массив для распечатки стиля с умолчаниями
+  let stl = $state({
+    "display": "flex",
+    "flex-direction": "row",
+    "justify-content": "flex-start",
+  });
+
+  //имя сласса для распечатки стиля
+  let class_name = ".flex-class {";
+</script>
+
+<div>
+  <div class="flex-show">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+  </div>
+
+  <div class="print-style">
+    <div>{@html class_name}</div>
+    {#each Object.keys(stl) as item, i}
+      <div>{item}:{stl[item]}</div>
+    {/each}
+    <div>}</div>
+  </div>
+</div>
+
+<style>
+  .flex-show {
+    display: var(--disp);
+    flex-direction: var(--direct);
+    justify-content: var(--justcont);
+    border: 1px solid silver;
+    width: 318px;
+  }
+  .print-style {
+    border: 1px solid silver;
+    width: 200px;
+    padding: 20px;
+    margin-top: 20px;;
+    background-color: aqua;
+  }
+</style>
